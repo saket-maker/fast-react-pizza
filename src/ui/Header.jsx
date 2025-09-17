f7eb394a70056cb639ea8abd3ef55c4f9d538b5a
Saket <saket.kumar2@ltimindtree.com>
2025-09-17 11:07:20 +0530

    test

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="flex justify-between border-b border-stone-300 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
-        Fast React Pizza Co.
+        Fast React Pizza Company
       </Link>
       <SearchOrder />
 
